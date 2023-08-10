0598e175b1faac9e7b1ce6453c79fdd2d9d58ae8
dopecello <williamlocke.cello@gmail.com>
2023-08-10 18:16:29 -0400

    added routing to simple API

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]  // this isn't strictly necessary, but it's a good idea to include it in order to improve performance
+    [Route("api/cities")]
+    public class CitiesController: ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "New York City" },
+                    new { id = 2, Name = "Baltimore" }
+                });
+        }
+    }
+}
